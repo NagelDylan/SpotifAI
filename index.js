ac7fb8a580dbba1aa619edb8358902fcb8d75eaf
NagelDylan <dylan.nagel2022@gmail.com>
2024-05-28 14:52:57 -0400

    remove profanity

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log("hello");
 const Song = require('./Song');
 
-const song1 = new Song('12345');
-song1.addGenre("penis monkey");
+const song1 = new Song('12345', 'howdy sir');
+song1.addGenre("monkey");
 
 song1.printGenres();
 
