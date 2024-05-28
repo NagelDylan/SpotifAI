266d9b94d0f803f7c0a58eca606ebd5e40b510cc
NagelDylan <dylan.nagel2022@gmail.com>
2024-05-28 14:58:39 -0400

    fixed thigns

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 console.log("hello");
-const Song = require('./Song');
+const { Song, VALENCE, ENERGY, DANCE, LIVELY } = require('./Song');
 
 const song1 = new Song('12345', 'howdy sir');
 song1.addGenre("monkey");
 
-song1.printGenres();
-
 song1.setValence(0.5);
-console.log(song1.getValence());
