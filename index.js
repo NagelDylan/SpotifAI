73c7f027d90082fa3081bd093097ba366d05fad2
Ben Petlach <ben.petlach@gmail.com>
2024-05-28 22:27:26 -0400

    double checked OOP

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //Just making sure that git is set up correctly.
-console.log("hello");
 const { Song, VALENCE, ENERGY, DANCE, LIVELY } = require('./Song');
 
 const song1 = new Song('12345', 'howdy sir');
 song1.addGenre("monkey");
 
 song1.setValence(0.5);
+
+console.log(song1.getGenres());
