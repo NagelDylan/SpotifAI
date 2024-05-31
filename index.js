f089d1aabd528a20ff356ef01fd3973683b6e347
NagelDylan <dylan.nagel2022@gmail.com>
2024-05-31 17:55:11 -0400

    removed random useless stuff for ben to test code

--- index.js
+++ index.js
@@ -20,29 +20,5 @@ main()
 
 
 const GENRE_FIND_LINK = "https://www.chosic.com/music-genre-finder/?track=";
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-async function scrapeData(id) {
-    const url = GENRE_FIND_LINK + id
-    const response = await axios.get(url);
-    const html = response.data;
-
-    console.log("hellO");
-    console.log(url);
-    console.log(html);
-
-    if (html) {
-        const $ = cheerio.load(html)
-
-        $('.pl-tags a').each((index, element) => {
-            const text = $(element).text();
-            console.log(text);
-        });
-    }
-    
-}
-
-
 
 song1.scrapeGenres();
