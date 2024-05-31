281acd8be6a627d1f65316e431087c5b7f2fb9ec
NagelDylan <dylan.nagel2022@gmail.com>
2024-05-31 18:02:23 -0400

    deleted typo and json file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Song, VALENCE, ENERGY, DANCE, LIVELY } = require('./Song');
 const { getDance, getEnergy, getLively, getValence } = require('./temp-sample-data/fileReader')
 
 
-const song1 = new Song('0pqnGHJpmpxLKifKRmU6WP', 'Believer');
+const song1 = new Song('6habFhsOp2NvshLv26DqMb', 'Despacito');
 
 song1.setValence(0.5);
 /*
