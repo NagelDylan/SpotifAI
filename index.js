9d90789dcf97044e6696217e789b0b8e67197c79
NagelDylan <dylan.nagel2022@gmail.com>
2024-06-02 12:27:23 -0400

    created algorithm draft folder

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { Song, VALENCE, ENERGY, DANCE, LIVELY } = require('./Song');
 const { getDance, getEnergy, getLively, getValence } = require('./temp-sample-data/fileReader')
 
-
 const song1 = new Song('6habFhsOp2NvshLv26DqMb', 'Despacito');
+
+const arr = [new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito'), new Song('6habFhsOp2NvshLv26DqMb', 'Despacito')]
 
 //Working example of setting/getting vibes
 song1.setVibes().then(() => {
     console.log(song1.getVibes())
-}) 
+})
 
 song1.setValence(0.5);
 
