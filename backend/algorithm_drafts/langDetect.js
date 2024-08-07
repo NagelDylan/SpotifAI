7508dccdf70c80c462bbaece901d08a6cbb53cab
NagelDylan <dylan.nagel2022@gmail.com>
2024-08-07 16:00:13 -0400

    added in langDetectFunction

--- backend/algorithm_drafts/langDetect.js
+++ backend/algorithm_drafts/langDetect.js
@@ -1,14 +1,20 @@
-import { detectAll } from 'tinyld';
+import { detect, detectAll } from "tinyld";
+import { getLyrics, getSong } from "genius-lyrics-api";
+import dotenv from "dotenv";
 
-//EXAMPLE:
-const textSimple = "This is a test lyric to see if the language detection is accurate. One language only"
-const textHard = `This is a test lyric to see if the language detection is accurate. Two languages!
-                    Ceci un test de paroles pour voir si la détection de la langue est exacte. 
-                    Il y a deux langues!`
-const textForeign = "Это тестовый текст, чтобы проверить точность определения языка. Только один язык"
+dotenv.config();
 
-console.log("Simple: ", detectAll(textSimple))
-console.log("Hard: ", detectAll(textHard))
-console.log("Foreign: ", detectAll(textForeign))
-//~~~~~~~
+const detectLang = async (song) => {
+  const options = {
+    apiKey: process.env.GENIUS_ACCESS_TOKEN,
+    title: song.title,
+    artist: song.artist,
+    optimizeQuery: true,
+  };
 
+  const lyrics = await getLyrics(options);
+  console.log(lyrics);
+  console.log(detectAll(lyrics));
+};
+
+export default detectLang
